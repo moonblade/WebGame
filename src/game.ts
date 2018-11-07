c72f75065f34e88fa4a2b9ed55167cc6e3c8a706
Mohammed Nisham <moonblade@Mohammeds-MacBook-Air.local>
2018-11-07 23:07:09 +0530

    webpack building Added building blocks for auto build using webpack with the script npm start

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,28 @@
+import * as ex from 'excalibur';
 class Game {
-    game: string;
+    game: ex.Engine;
     constructor() {
-        this.game = "test";
+        this.game = new ex.Engine({
+            width: 800,
+            height: 600
+        });
+
+        var paddle = new ex.Actor(150, this.game.drawHeight - 40, 200, 20)
+
+paddle.color = ex.Color.Chartreuse
+
+// Make sure the paddle can partipate in collisions, by default excalibur actors do not collide
+paddle.collisionType = ex.CollisionType.Fixed
+
+// `game.add` is the same as calling
+// `game.currentScene.add`
+this.game.add(paddle)
+
+    }
+
+    start() {
+        this.game.start()
     }
 }
+
+export default Game;
