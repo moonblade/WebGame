ec6f7e0d380c932b569dd32fe5569fd893da929c
Mohammed Nisham <moonblade@Mohammeds-MacBook-Air.local>
2018-11-08 20:23:20 +0530

    livereload added

--- src/game.ts
+++ src/game.ts
@@ -1,23 +1,13 @@
 import * as ex from 'excalibur';
+import Player from './player';
 class Game {
     game: ex.Engine;
+    player: Player;
     constructor() {
-        this.game = new ex.Engine({
-            width: 800,
-            height: 600
-        });
+        this.game = new ex.Engine();
 
-        var paddle = new ex.Actor(150, this.game.drawHeight - 40, 200, 20)
-
-paddle.color = ex.Color.Chartreuse
-
-// Make sure the paddle can partipate in collisions, by default excalibur actors do not collide
-paddle.collisionType = ex.CollisionType.Fixed
-
-// `game.add` is the same as calling
-// `game.currentScene.add`
-this.game.add(paddle)
-
+        this.player = new Player();
+        this.game.add(this.player);
     }
 
     start() {
---
+++ src/player.ts
@@ -0,0 +1,8 @@
+import * as ex from 'excalibur';
+class Player extends ex.Actor{
+    constructor() {
+        super();
+    }
+}
+
+export default Player;
