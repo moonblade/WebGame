f1b5ad1aff9404a28f980eb7dc439ac606a91a2e
moonblade <moonblade168@gmail.com>
2018-11-10 21:59:00 +0530

    added movement with mouse for player

--- src/player.ts
+++ src/player.ts
@@ -2,16 +2,27 @@ import { Actor, Engine, Color, Texture, Sprite } from 'excalibur';
 import Resources from './resources';
 class Player extends Actor {
     health: number;
+    speed: number;
     sprite: Sprite;
 
     constructor() {
         super(30,30,30,30);
         this.color = Color.Red;
+        this.speed = 600;
     }
     
+    clicked(coordinates:any) {
+        this.actions.moveTo(coordinates.x, coordinates.y, this.speed);
+    }
+
     public onInitialize(engine: Engine) {
         this.sprite = new Sprite(Resources.getInstance().walkTexture, 0, 0, 120, 120);
         this.addDrawing(this.sprite)
+        this.enableCapturePointer = true;
+        // respond to click events
+        engine.input.pointers.primary.on('down', (event:any)=>{
+            this.clicked(event.coordinates.worldPos);
+        })
     }
 
 }
