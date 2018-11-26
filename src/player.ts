e35483e5186e8c17798145e97acd098b81ef3b74
Mohammed Nisham <moonblade@Mohammeds-MacBook-Air.local>
2018-11-26 20:45:18 +0530

    changes

--- src/player.ts
+++ src/player.ts
@@ -1,4 +1,4 @@
-import { Actor, Engine, Color, Texture, Sprite, Vector, SpriteSheet, Animation, ConsoleAppender, EnterTriggerEvent, Input, CollisionType } from 'excalibur';
+import { Actor, Engine, Color, Texture, Sprite, Vector, SpriteSheet, Animation, ConsoleAppender, EnterTriggerEvent, Input, CollisionType, Cell } from 'excalibur';
 import Resources from './resources';
 import { TiledResource } from './lib/tiled';
 
@@ -7,22 +7,18 @@ class Player extends Actor {
     speed: number;
     tiledResource: TiledResource;
     // How much travel in one update
-    keyboardSpeed: number;
+    keyboardSpeed: Vector;
     idleSprite: Sprite;
     
     constructor(tiledResource: TiledResource) {
         super(30,30,30,30);
         this.color = Color.Red;
         this.speed = 500;
-        this.keyboardSpeed = 1;
+        this.keyboardSpeed = new Vector(14, 21);
         this.tiledResource = tiledResource;
         this.collisionType = CollisionType.Active;
 
         this.tiledResource.getTileMap().getCell(2,1).solid = true
-        console.log(this.tiledResource.data.layers[0].data)
-        console.log(this.tiledResource.getTileMap().getCell(2,1).sprites)
-        console.log(this.tiledResource.getTileMap().getCell(2,1).solid)
-        console.log(this.tiledResource.getTileMap().getCell(3,2).solid)
     }
 
     moveRight(coordinate: Vector) {
@@ -59,7 +55,13 @@ class Player extends Actor {
 
     async moveTo(coordinate:any, pathFind:boolean = true) {
         // TODO: Path finding algorithm required to move around obstacles in the room
-        console.log(this.tiledResource.getTileMap().collides(this));
+        let cell:Cell = this.tiledResource.getTileMap().getCellByPoint(coordinate.x, coordinate.y)
+        if(!cell || (cell && cell.solid && !pathFind)) {
+            this.actions.clearActions();
+            return;
+        }
+        if (this.tiledResource.getTileMap().collides(this))
+            return;
         this.actions.clearActions();
         if (this.pos.x < coordinate.x) {
             await this.moveRight(coordinate);
@@ -76,23 +78,22 @@ class Player extends Actor {
     }
 
     clicked(coordinate:any) {
-        console.log(this.tiledResource.getTileMap().getCellByPoint(coordinate.x, coordinate.y).solid)
         this.moveTo(coordinate);
     }
 
     update(engine: Engine, delta: number) {
         super.update(engine, delta);
         if (engine.input.keyboard.isHeld(Input.Keys.W) || engine.input.keyboard.isHeld(Input.Keys.Up)) {
-            this.moveTo(new Vector(this.pos.x, this.pos.y - this.keyboardSpeed), false);
+            this.moveTo(new Vector(this.pos.x, this.pos.y - this.keyboardSpeed.y), false);
         }
         if (engine.input.keyboard.isHeld(Input.Keys.A) || engine.input.keyboard.isHeld(Input.Keys.Left)) {
-            this.moveTo(new Vector(this.pos.x - this.keyboardSpeed, this.pos.y), false);
+            this.moveTo(new Vector(this.pos.x - this.keyboardSpeed.x, this.pos.y), false);
         }
         if (engine.input.keyboard.isHeld(Input.Keys.S) || engine.input.keyboard.isHeld(Input.Keys.Down)) {
-            this.moveTo(new Vector(this.pos.x, this.pos.y + this.keyboardSpeed), false);
+            this.moveTo(new Vector(this.pos.x, this.pos.y + this.keyboardSpeed.y), false);
         }
         if (engine.input.keyboard.isHeld(Input.Keys.D) || engine.input.keyboard.isHeld(Input.Keys.Right)) {
-            this.moveTo(new Vector(this.pos.x + this.keyboardSpeed, this.pos.y), false);
+            this.moveTo(new Vector(this.pos.x + this.keyboardSpeed.x, this.pos.y), false);
         }
     }
 
