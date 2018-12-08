b3b8c55366d77821c619bc0ff35eb58c67a282d5
moonblade <moonblade168@gmail.com>
2018-12-08 22:53:19 +0530

    bottles

--- resources/map/items.py
+++ resources/map/items.py
@@ -5,6 +5,6 @@ files = os.popen('ls -v ~/Downloads/pixil-frame-0\ *.png | sort -n -k2.3').read(
 files = files.split('\n')
 # files = [files.split('\n')[-1]] + files.split('\n')[:-1]
 files = ' '.join(files).replace(" (", "\ (").replace("(", "\(").replace(")", "\)")
-print(files)
+# print(files)
 command = 'convert ' + files + ' +append ' + outfile
 os.system(command)
