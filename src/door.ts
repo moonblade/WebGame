75c7d47f020b3bda58cb5cf01f6d291f151c276c
moonblade <moonblade168@gmail.com>
2019-01-04 21:55:44 +0530

    doors

--- src/door.ts
+++ src/door.ts
@@ -1,4 +1,4 @@
-import { Actor, Sprite, CollisionType, CollisionStartEvent } from "excalibur";
+import { Actor, Sprite, CollisionType, CollisionStartEvent, CollisionEndEvent } from "excalibur";
 import Resources from "./resources";
 import Game from "./game";
 import Player from "./player";
@@ -9,15 +9,18 @@ class Door extends Actor{
     spriteName: string;
     name: string;
     sprite: Sprite;
+    spriteLocked: Sprite;
     key: string;
+    locked: boolean;
 
     constructor(properties: any) {
         super(properties);
-        this.type = properties.type
-        this.spriteName = properties.type
-        this.name = properties.name || properties.type
-        this.collisionType = CollisionType.Fixed;
+        this.type = properties.type;
+        this.spriteName = properties.type;
+        this.name = properties.name || properties.type + Math.random();
         this.key = properties.key;
+        this.locked = properties.locked || this.key != "default";
+        this.collisionType = this.locked?CollisionType.Fixed:CollisionType.Passive;
     }
 
     static initialize(properties: any, addToGame: boolean) {
@@ -29,22 +32,40 @@ class Door extends Actor{
 
     collisionStart(event:CollisionStartEvent):void{
         if (event.other == Player.getInstance()) {
-            let key:Entity = Player.getInstance().getInventory().getSelectedItem();
-            if (key && key.name == this.key) {
-                key.place();
-                key.remove();
-                Game.getInstance().remove(this);
+            if (this.locked) {
+                let key:Entity = Player.getInstance().getInventory().getSelectedItem();
+                if (key && key.name == this.key) {
+                    this.locked = false;
+                    key.place();
+                    key.remove();
+                    this.collisionType = CollisionType.Passive;
+                    this.setDrawing("unlocked");
+                    this.visible = false;
+                }
+            } else {
+                this.visible = false;
             }
         }
     }
 
+    collisionEnd(event:any): void {
+        this.visible = true;
+    }
+
     onInitialize() {
         this.sprite = Resources.getInstance().getSprite(this.spriteName)
+        this.spriteLocked = Resources.getInstance().getSprite(this.spriteName + "Locked")
         if (this.sprite) {
-            this.addDrawing(this.sprite);
+            this.addDrawing("unlocked", this.sprite);
+            this.addDrawing("locked", this.spriteLocked);
+            if (this.locked)
+                this.setDrawing("locked");
+            else
+                this.setDrawing("unlocked");
             this.setWidth(this.sprite.width);
             this.setHeight(this.sprite.height);
             this.on("collisionstart", this.collisionStart);
+            this.on("collisionend", this.collisionEnd);
         }        
     }
 
